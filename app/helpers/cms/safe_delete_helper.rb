5038420747018a3c49cfc5e8e534867353f4c4ae
Andrey Bakuta <anb@anahoret.com>
2016-04-26 11:57:48 +0300

    Change show_deleted helper to merge params.

--- app/helpers/cms/safe_delete_helper.rb
+++ app/helpers/cms/safe_delete_helper.rb
@@ -2,9 +2,9 @@ module Cms
   module SafeDeleteHelper
     def switch_deleted_link
       if params[:show] == 'all'
-        link_to t('admin.hide_deleted'), show: nil
+        link_to t('admin.hide_deleted'), url_for(params.merge(show: nil))
       else
-        link_to t('admin.show_deleted'), show: 'all'
+        link_to t('admin.show_deleted'), url_for(params.merge(show: 'all'))
       end
     end
   end
