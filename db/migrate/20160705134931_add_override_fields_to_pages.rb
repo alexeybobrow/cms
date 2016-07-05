3dc7854b50c0fe7f3fa48c1ef53f85d04b444c7b
Andrey Bakuta <anb@anahoret.com>
2016-07-05 18:25:48 +0300

    Change column defaults.

--- db/migrate/20160705134931_add_override_fields_to_pages.rb
+++ db/migrate/20160705134931_add_override_fields_to_pages.rb
@@ -1,7 +1,7 @@
 class AddOverrideFieldsToPages < ActiveRecord::Migration
   def change
-    add_column :pages, :override_name, :boolean, default: false
-    add_column :pages, :override_title, :boolean, default: false
-    add_column :pages, :override_breadcrumb_name, :boolean, default: false
+    add_column :pages, :override_name, :boolean, default: true
+    add_column :pages, :override_title, :boolean, default: true
+    add_column :pages, :override_breadcrumb_name, :boolean, default: true
   end
 end
