7e859a838cd8a15f240d0432e5020a93ada3d717
dra1n <dra1n86@gmail.com>
2016-07-11 14:48:08 +0300

    WIP.

--- app/models/url.rb
+++ app/models/url.rb
@@ -1,3 +1,13 @@
 class Url < ActiveRecord::Base
+  include Cms::Populator
+
   belongs_to :page
+
+  delegate :body, to: :page
+
+  populate with: Cms::PagePropPopulator::Url, from: :body do |text, model|
+    if text.present?
+      model.name = text
+    end
+  end
 end
