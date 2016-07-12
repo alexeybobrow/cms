78b82e9885a8852d46e187a600651ced6468f375
dra1n <dra1n86@gmail.com>
2016-07-12 15:11:56 +0300

    Remove migrations.

--- db/migrate/20160711101756_add_prefix_to_url.rb
+++
@@ -1,6 +0,0 @@
-class AddPrefixToUrl < ActiveRecord::Migration
-  def change
-    add_column :urls, :prefix, :string, default: ''
-    add_column :urls, :use_prefix, :boolean, default: true
-  end
-end
