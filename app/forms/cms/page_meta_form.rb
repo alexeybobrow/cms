3f1226ebf2f6ff9ef3a8684ac41e7a01c3427bdb
dra1n <dra1n86@gmail.com>
2016-08-19 14:39:45 +0300

    Meta form update.

--- app/forms/cms/page_meta_form.rb
+++ app/forms/cms/page_meta_form.rb
@@ -2,6 +2,10 @@ module Cms
   class PageMetaForm < ::Cms::BaseModelForm
     model Page
 
+    META_REGULAR = Set['property', 'content']
+    META_OPEN_GRAPH = Set['name', 'content']
+    META_GROUPS = [META_REGULAR, META_OPEN_GRAPH]
+
     extend ArrayAccessors
 
     attribute :title
@@ -23,15 +27,30 @@ module Cms
       normalize_meta!
     end
 
+    def grouped_meta
+      self.meta.reduce({}) do |acc, meta|
+        (acc[meta.keys.to_set] ||= []) << meta; acc
+      end
+    end
+
+    def meta_hash(properties)
+      Hash[properties.to_a.zip((0..properties.size).map{''})]
+    end
+
+    def meta_index
+      @index ||= 0
+      @index += 1
+    end
+
     private
 
     def skip_val?(value)
-      '1' == value['_destroy'] || value['property'].blank? || value['content'].blank?
+      '1' == value['_destroy'] || value.values.any?(&:blank?)
     end
 
     def normalize_meta!
       meta_values = meta.is_a?(Array) ? meta : meta.values
-      self.meta = meta_values.reject {|v| skip_val?(v) }.map {|v| v.slice('property', 'content')}
+      self.meta = meta_values.reject { |v| skip_val?(v) }.map { |v| v.except('_destroy') }
     end
 
     def before_save
