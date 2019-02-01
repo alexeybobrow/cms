350dd8c147fd455f676939dff483062938eca619
Eduard Kolenov <kolenov.ed@gmail.com>
2019-02-01 03:01:48 +0200

    html renderer refactoring

--- lib/cms/liquid/html_renderer.rb
+++ lib/cms/liquid/html_renderer.rb
@@ -12,6 +12,7 @@ module Cms
             out(:children)
           end
         end
+        @attrs = ''
       end
 
       def link(node)
@@ -25,34 +26,55 @@ module Cms
             end
           end
           node.next.string_content = ''
-        end
+        end unless node.next.nil?
+
         out('<a href="', node.url.nil? ? '' : escape_href(node.url), '"')
         if node.title && !node.title.empty?
           out(' title="', escape_html(node.title), '"')
         end
-        out("#{@attrs}>", :children, '</a>')
+        out("#{' ' << @attrs unless @attrs.empty?}>", :children, '</a>')
+        @attrs = ''
       end
 
       def paragraph(node)
         if is_liquid_tag?(node.to_html)
           out(:children)
         elsif is_html_attr?(node.to_html)
-          @attrs = html_attr_to_s?(node.to_html)
-          if node.first_child.next === nil
+
+          # store @attrs for header and return :header node
+          if node.first_child.next.nil?
+            @attrs = html_attr_to_s?(node.to_html)
             node.first_child.delete
-          elsif @in_tight && node.parent.type != :blockquote
-              out(:children)
-            else
-              block do
-                container("<p#{sourcepos(node)}>", '</p>') do
-                  out(:children)
-                  if node.parent.type == :footnote_definition && node.next.nil?
-                    out(" ")
-                    out_footnote_backref
-                  end
+            return out(:children)
+
+          # store @attrs for paragraph and return :paragraph node
+          elsif node.first_child.type === :text && is_html_attr?(node.first_child.string_content)
+            @attrs = html_attr_to_s?(node.first_child.string_content)
+            node.first_child.delete
+
+          # store @attrs for link and return :link node
+          elsif node.first_child.type === :link
+            if node.last_child.type === :text && is_html_attr?(node.last_child.string_content)
+              @attrs = html_attr_to_s?(node.last_child.string_content)
+              node.last_child.delete
+            end
+            return out(:children)
+          end
+
+          if @in_tight && node.parent.type != :blockquote
+            out(:children)
+          else
+            block do
+              container("<p#{sourcepos(node)}#{' ' << @attrs unless @attrs.empty?}>", '</p>') do
+                out(:children)
+                if node.parent.type == :footnote_definition && node.next.nil?
+                  out(" ")
+                  out_footnote_backref
                 end
               end
             end
+            @attrs = ''
+          end
         else
           super
         end
@@ -61,9 +83,19 @@ module Cms
       private
 
       # Example:
-      #   "{% layout width: 600px %}"
+      #   "<p>{% layout width: 600px %}</p>"
+      #
+      #   Or
+      #   "<p>{% herounit color: red %}
+      #   {% herotitle Ruby on Rails %}
+      #   {% heroimage hero.png %}
+      #   {% herodescription %}
+      #     Anadea has state-of-the art expertise in Ruby on Rails development.
+      #   {% endherodescription %}
+      #   {% herobutton 'Contact Us', url: /contact-us %}
+      #   {% endherounit %}</p>"
       def is_liquid_tag?(string)
-        /{%.*%}/ === string
+        /<p>\s*{%.*%}+\s*<\/p>/m === string
       end
 
       # Example:
@@ -73,7 +105,7 @@ module Cms
       end
 
       def html_attr_to_s?(str)
-        HtmlAttributesParser.transform(str: str).map { |key, val| "#{key}='#{val}'" }.join(' ')
+        HtmlAttributesParser.transform(str: str).map { |key, val| "#{key}=\"#{val}\"" }.join(' ')
       end
     end
   end
