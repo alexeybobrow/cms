94aa3aa650014f0073f09c304d030cc78e145b83
Eduard Kolenov <kolenov.ed@gmail.com>
2019-03-27 19:20:01 +0200

    add test

--- spec/controllers/public/rates_controller_spec.rb
+++ spec/controllers/public/rates_controller_spec.rb
@@ -9,7 +9,12 @@ RSpec.describe ::Cms::Public::RatesController, type: :controller do
   end
 
   it 'create' do
-    expect{ post :create, {page_id: @page.id, rate: 5} }.to change{Rate.count}.by(1)
+    expect { post :create, { page_id: @page.id, rate: 5 } }.to change { Rate.count }.by(1)
     expect(response).to redirect_to "where_i_came_from"
+  end
+
+  it 'create_json' do
+    expect { post :create, { page_id: @page.id, rate: 5, format: :json } }.to change { Rate.count }.by(1)
+    expect(JSON.parse(response.body)).to eq({ "rating" => 5.0, "votes" => 1 })
   end
 end
