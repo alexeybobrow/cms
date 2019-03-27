855e5bb4f35d23774891784ae306ec1e4f6f1834
Eduard Kolenov <kolenov.ed@gmail.com>
2019-03-27 17:53:45 +0200

    replace redirect by render json

--- app/controllers/cms/public/rates_controller.rb
+++ app/controllers/cms/public/rates_controller.rb
@@ -3,13 +3,13 @@ module Cms
     class RatesController < ::Cms::Public::BaseController
       def create
         session[:user_rated_posts] ||= []
-        
+
         unless session[:user_rated_posts].include?(params[:page_id])
-          Rate.create(page_id: params[:page_id], value: params[:rate])
+          @rate = Rate.create(page_id: params[:page_id], value: params[:rate])
           session[:user_rated_posts] << params[:page_id]
         end
 
-        redirect_to :back
+        render :json => { rating: @rate.page.average_rate, votes: @rate.page.rates&.size }
       end
     end
   end
