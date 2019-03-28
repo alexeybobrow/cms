78fbc2044414f5b03ca2606dd3a2b738638247d1
Eduard Kolenov <kolenov.ed@gmail.com>
2019-03-28 19:50:54 +0200

    refactoring

--- app/controllers/cms/public/rates_controller.rb
+++ app/controllers/cms/public/rates_controller.rb
@@ -9,13 +9,24 @@ module Cms
           session[:user_rated_posts] << params[:page_id]
         end
 
-        respond_to do |format|
-          format.html {
-            redirect_to :back
-          }
-          format.json {
-            render :json => { rating: @rate.page.average_rate, votes: @rate.page.rates.size }
-          }
+        if @rate.valid?
+          respond_to do |format|
+            format.html do
+              redirect_to :back
+            end
+            format.json do
+              render json: { rating: @rate.page.average_rate, votes: @rate.page.rates.size }
+            end
+          end
+        else
+          respond_to do |format|
+            format.html do
+              head :unprocessable_entity
+            end
+            format.json do
+              render json: { error: @rate.errors.full_messages }, status: :unprocessable_entity
+            end
+          end
         end
       end
     end
--- spec/controllers/public/rates_controller_spec.rb
+++ spec/controllers/public/rates_controller_spec.rb
@@ -3,18 +3,44 @@ require 'spec_helper'
 RSpec.describe ::Cms::Public::RatesController, type: :controller do
   routes { Cms::Engine.routes }
 
+  let(:page) { create :page, :blog, :draft, url: '/blog/article', content_body: 'Article About Us' }
+
   before do
-    @page = create :page, :blog, :draft, url: '/blog/article', content_body: 'Article About Us'
-    request.env["HTTP_REFERER"] = "where_i_came_from"
+    request.env['HTTP_REFERER'] = 'where_i_came_from'
   end
 
-  it 'create' do
-    expect { post :create, { page_id: @page.id, rate: 5 } }.to change { Rate.count }.by(1)
-    expect(response).to redirect_to "where_i_came_from"
+  describe 'Success' do
+    context 'html' do
+      it 'create' do
+        expect { post :create, page_id: page.id, rate: 5 }.to change { Rate.count }.by(1)
+        expect(response).to redirect_to 'where_i_came_from'
+      end
+    end
+
+    context 'json' do
+      it 'create' do
+        expect { post :create, page_id: page.id, rate: 5, format: :json }.to change { Rate.count }.by(1)
+        expect(response.content_type).to eq('application/json')
+        expect(JSON.parse(response.body)).to eq('rating' => 5.0, 'votes' => 1)
+      end
+    end
   end
 
-  it 'create_json' do
-    expect { post :create, { page_id: @page.id, rate: 5, format: :json } }.to change { Rate.count }.by(1)
-    expect(JSON.parse(response.body)).to eq({ "rating" => 5.0, "votes" => 1 })
+  describe 'Fail' do
+    context 'html' do
+      it 'fails' do
+        expect { post :create, page_id: page.id, rate: 0 }.not_to change { Rate.count }
+        expect(response).to have_http_status(:unprocessable_entity)
+      end
+    end
+
+    context 'json' do
+      it 'fails' do
+        expect { post :create, page_id: page.id, rate: 0, format: :json }.not_to change { Rate.count }
+        expect(response.content_type).to eq('application/json')
+        expect(response).to have_http_status(:unprocessable_entity)
+        expect(JSON.parse(response.body)).to have_key('error')
+      end
+    end
   end
 end
