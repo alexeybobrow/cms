a35da37963c718c836035bc0ce67b474e931e27e
Alice Kondratieva <ak@anahoret.com>
2019-04-18 09:40:29 +0300

    Improve case when user rates the same page twice, add spec

--- app/controllers/cms/public/rates_controller.rb
+++ app/controllers/cms/public/rates_controller.rb
@@ -4,30 +4,28 @@ module Cms
       def create
         session[:user_rated_posts] ||= []
 
-        unless session[:user_rated_posts].include?(params[:page_id])
-          @rate = Rate.create(page_id: params[:page_id], value: params[:rate])
-          session[:user_rated_posts] << params[:page_id]
-        end
-
-        if @rate&.valid?
-          responce_success(@rate)
+        if already_rated?
+          response_error
         else
-          responce_error(@rate)
+          rate = Rate.new(page_id: params[:page_id], value: params[:rate])
+          rate.save ? response_success(rate) : response_error(rate)
         end
       end
 
       private
 
-      def responce_success(rate)
+      def response_success(rate)
+        session[:user_rated_posts] << params[:page_id].to_s
+
         respond_to do |format|
           format.html { redirect_to :back }
           format.json { render json: { rating: rate.page.average_rate, votes: rate.page.rates.size } }
         end
       end
 
-      def responce_error(rate)
+      def response_error(rate = nil)
         respond_to do |format|
-          if @rate.present?
+          if rate.present?
             format.html { head :unprocessable_entity }
             format.json { render json: { error: rate.errors.full_messages }, status: :unprocessable_entity }
           else
@@ -35,6 +33,10 @@ module Cms
             format.json { head :forbidden }
           end
         end
+      end
+
+      def already_rated?
+        session[:user_rated_posts].include?(params[:page_id].to_s)
       end
     end
   end
--- spec/controllers/public/rates_controller_spec.rb
+++ spec/controllers/public/rates_controller_spec.rb
@@ -10,36 +10,47 @@ RSpec.describe ::Cms::Public::RatesController, type: :controller do
   end
 
   describe 'Success' do
-    context 'html' do
-      it 'create' do
-        expect { post :create, page_id: page.id, rate: 5 }.to change { Rate.count }.by(1)
-        expect(response).to redirect_to 'where_i_came_from'
-      end
+    let(:session) { {} }
+
+    before do
+      allow_any_instance_of(described_class).to receive(:session).and_return(session)
     end
 
-    context 'json' do
-      it 'create' do
-        expect { post :create, page_id: page.id, rate: 5, format: :json }.to change { Rate.count }.by(1)
-        expect(response.content_type).to eq('application/json')
-        expect(JSON.parse(response.body)).to eq('rating' => 5.0, 'votes' => 1)
-      end
+    it 'creates rate for html' do
+      expect { post :create, page_id: page.id, rate: 5 }.to change { Rate.count }.by(1)
+      expect(response).to redirect_to 'where_i_came_from'
+      expect(session[:user_rated_posts]).to include(page.id.to_s)
+    end
+
+    it 'creates rate for json' do
+      expect { post :create, page_id: page.id, rate: 5, format: :json }.to change { Rate.count }.by(1)
+      expect(response.content_type).to eq('application/json')
+      expect(JSON.parse(response.body)).to eq('rating' => 5.0, 'votes' => 1)
+      expect(session[:user_rated_posts]).to include(page.id.to_s)
     end
   end
 
   describe 'Fail' do
-    context 'html' do
-      it 'fails' do
+    context 'user tries to rate with zero rate' do
+      it 'fails in html' do
         expect { post :create, page_id: page.id, rate: 0 }.not_to change { Rate.count }
         expect(response).to have_http_status(:unprocessable_entity)
       end
-    end
 
-    context 'json' do
-      it 'fails' do
+      it 'fails in json' do
         expect { post :create, page_id: page.id, rate: 0, format: :json }.not_to change { Rate.count }
         expect(response.content_type).to eq('application/json')
         expect(response).to have_http_status(:unprocessable_entity)
         expect(JSON.parse(response.body)).to have_key('error')
+      end
+    end
+
+    context 'user tries to rate the same page twice' do
+      it 'fails' do
+        params = { page_id: page.id, rate: 5 }
+        session = { user_rated_posts: [page.id.to_s] }
+        expect { post :create, params, session }.not_to change { Rate.count }
+        expect(response).to have_http_status(:forbidden)
       end
     end
   end
